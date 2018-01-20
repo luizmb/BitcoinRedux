85765b0e03f8c4c3a65bf62542d150075f47b320
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-01-20 21:48:52 +0100

    100% coverage for all foundation extensions

--- N26/N26Tests/Common/CocoaExTests.swift
+++ N26/N26Tests/Common/CocoaExTests.swift
@@ -16,6 +16,49 @@ class AnotherView: UIView { }
 class AnotherViewController: UIViewController { }
 
 class CocoaExTests: UnitTest {
+    func testArraySafeSubscript() {
+        let array = [1, 2, 3, 5, 8, 13]
+        XCTAssertNil(array[safe: 6])
+        XCTAssertNil(array[safe: 7])
+        XCTAssertNil(array[safe: 8])
+        XCTAssertNil(array[safe: -1])
+        XCTAssertNotNil(array[safe: 0])
+        XCTAssertNotNil(array[safe: 1])
+        XCTAssertNotNil(array[safe: 2])
+        XCTAssertNotNil(array[safe: 3])
+        XCTAssertNotNil(array[safe: 4])
+        XCTAssertNotNil(array[safe: 5])
+        XCTAssertEqual(array[safe: 0], 1)
+        XCTAssertEqual(array[safe: 1], 2)
+        XCTAssertEqual(array[safe: 2], 3)
+        XCTAssertEqual(array[safe: 3], 5)
+        XCTAssertEqual(array[safe: 4], 8)
+        XCTAssertEqual(array[safe: 5], 13)
+    }
+
+    func testDateBackToMidnight() {
+        let f = DateFormatter()
+        f.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+        XCTAssertEqual(f.date(from: "2010-09-15 09:20:10")!.backToMidnight,
+                       f.date(from: "2010-09-15 00:00:00")!)
+        XCTAssertEqual(Date(timeIntervalSince1970: 388416300).backToMidnight,
+                       Date(timeIntervalSince1970: 388360800))
+    }
+
+    func testDateAddingDays() {
+        let f = DateFormatter()
+        f.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        XCTAssertEqual(f.date(from: "2010-09-15 09:20:10")!.addingDays(7),
+                       f.date(from: "2010-09-22 09:20:10")!)
+        XCTAssertEqual(f.date(from: "2010-09-15 09:20:10")!.addingDays(-14),
+                       f.date(from: "2010-09-01 09:20:10")!)
+        XCTAssertEqual(Date(timeIntervalSince1970: 388416300).addingDays(2),
+                       Date(timeIntervalSince1970: 388416300 + 2 * 24 * 60 * 60))
+        XCTAssertEqual(Date(timeIntervalSince1970: 388416300).addingDays(-7),
+                       Date(timeIntervalSince1970: 388416300 - 7 * 24 * 60 * 60))
+    }
+
     func testReuseIdentifier() {
         XCTAssertEqual(UICollectionReusableView.reuseIdentifier(), "UICollectionReusableView")
         XCTAssertEqual(AnotherCollectionReusableView.reuseIdentifier(), "AnotherCollectionReusableView")
@@ -28,6 +71,17 @@ class CocoaExTests: UnitTest {
         XCTAssertEqual(AnotherView.nibName, "AnotherView")
         XCTAssertEqual(UIViewController.nibName, "UIView")
         XCTAssertEqual(AnotherViewController.nibName, "AnotherView")
+    }
+
+    func testNib() {
+        let vc = HistoricalViewController()
+        XCTAssertTrue(HistoricalView.nib().instantiate(withOwner: vc, options: nil).first as? HistoricalView != nil)
+        XCTAssertTrue(HistoricalViewController.nib().instantiate(withOwner: vc, options: nil).first as? HistoricalView != nil)
+    }
+
+    func testStartable() {
+        let sut = HistoricalViewController.start()
+        XCTAssertNotNil(sut)
     }
 
     func testWindowHelpers() {
