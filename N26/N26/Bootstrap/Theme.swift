e473870542acd41937eb4022a5b896d64f3e2765
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-01-20 18:41:42 +0100

    Set colors from theme (using asset catalog colors)

--- N26/N26/Bootstrap/Theme.swift
+++ N26/N26/Bootstrap/Theme.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
+extension UIColor {
+    enum Named: String {
+        case chromeBackground = "ChromeBackground"
+        case chromeText = "ChromeText"
+
+        var color: UIColor {
+            return .of(self)
+        }
+    }
+
+    static func of(_ named: Named) -> UIColor {
+        return UIColor(named: named.rawValue)!
+    }
+}
+
 final class Theme {
+
     static func apply() {
         let navigationBarAppearance = UINavigationBar.appearance()
-        navigationBarAppearance.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.yellow]
+        navigationBarAppearance.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.of(.chromeText)]
         navigationBarAppearance.barStyle = .black
-        navigationBarAppearance.barTintColor = .red
-        navigationBarAppearance.tintColor = .yellow
+        navigationBarAppearance.barTintColor = .of(.chromeBackground)
+        navigationBarAppearance.tintColor = .of(.chromeText)
         navigationBarAppearance.isTranslucent = false
         navigationBarAppearance.setBackgroundImage(UIImage(), for: .default)
         navigationBarAppearance.shadowImage = UIImage()
