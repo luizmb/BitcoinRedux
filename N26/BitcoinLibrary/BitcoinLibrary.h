1a9022ffdbb5e98bde2400ac98ad6b1ba4d653f0
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-01-21 15:29:18 +0100

    Create targets for shared bitcoin library

---
+++ N26/BitcoinLibrary/BitcoinLibrary.h
@@ -0,0 +1,19 @@
+//
+//  BitcoinLibrary.h
+//  BitcoinLibrary
+//
+//  Created by Luiz Rodrigo Martins Barbosa on 21.01.18.
+//  Copyright © 2018 Luiz Rodrigo Martins Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitcoinLibrary.
+FOUNDATION_EXPORT double BitcoinLibraryVersionNumber;
+
+//! Project version string for BitcoinLibrary.
+FOUNDATION_EXPORT const unsigned char BitcoinLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitcoinLibrary/PublicHeader.h>
+
+
